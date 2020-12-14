5d50e73847f8c9332e0cce5033a0d04fbfb6ca1e
wenner <nelsondiasdemedeiros@gmail.com>
2020-12-14 15:25:26 -0300

    add component Rating

---
+++ app/javascript/src/components/common/Rating/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default (props) => {
+  const score = (props.score / 5) * 100
+
+  return (
+    <span className="star-wrapper">
+      <span className="stars" style={{ width: score + "%" }}></span>
+    </span>
+  )
+}
