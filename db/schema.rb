64c18b1815514c0a0d59d3690fbbe69e1bb8da02
wenner <nelsondiasdemedeiros@gmail.com>
2020-12-15 14:13:38 -0300

    feat: add user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_11_143354) do
+ActiveRecord::Schema.define(version: 2020_12_15_170515) do
 
   create_table "airlines", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 2020_12_11_143354) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["airline_id"], name: "index_reviews_on_airline_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "reviews", "airlines"
