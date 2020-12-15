0655d132c38ce53ee7a68e12244af88b4e88b8bd
wenner <nelsondiasdemedeiros@gmail.com>
2020-12-15 13:47:02 -0300

    feat: add component Reset

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom'
 import { Provider } from './providers/Provider'
 import UnprotectedRoute from './UnprotectedRoute'
 import Forgot from './Auth/Password/Forget'
+import Reset from './Auth/Password/Reset'
 import { Header } from './theme/Header'
 import Register from './Auth/Register'
 import Airlines from './Airlines'
@@ -19,6 +20,7 @@ export default () => (
       <UnprotectedRoute path="/login" component={ Login }/>
       <UnprotectedRoute exact path="/register" component={ Register } />
       <UnprotectedRoute path="/forgot-password" component={ Forgot }/>
+      <UnprotectedRoute path="/reset-password" component={ Reset }/>
     </Switch> 
   </Provider>
 )
