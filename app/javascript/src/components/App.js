4b18e8cacc5633c73ebbac36ec4a2ed326437b55
wenner <nelsondiasdemedeiros@gmail.com>
2020-12-16 13:44:10 -0300

    refactor: remove some components

--- app/javascript/src/components/App.js
+++ app/javascript/src/components/App.js
@@ -3,8 +3,6 @@ import './styles.css'
 import { Route, Switch } from 'react-router-dom'
 import { Provider } from './providers/Provider'
 import UnprotectedRoute from './UnprotectedRoute'
-import Forgot from './Auth/Password/Forget'
-import Reset from './Auth/Password/Reset'
 import { Header } from './theme/Header'
 import Register from './Auth/Register'
 import Airlines from './Airlines'
@@ -19,8 +17,6 @@ export default () => (
       <Route exact path="/airlines/:slug" component={ Airline } />
       <UnprotectedRoute path="/login" component={ Login }/>
       <UnprotectedRoute exact path="/register" component={ Register } />
-      <UnprotectedRoute path="/forgot-password" component={ Forgot }/>
-      <UnprotectedRoute path="/reset-password" component={ Reset }/>
     </Switch> 
   </Provider>
 )
